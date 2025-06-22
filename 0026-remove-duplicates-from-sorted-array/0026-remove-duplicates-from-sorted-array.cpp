b912c2b227b877d108e3630240b39381a8855a91
ded2322 <otheremails602@gmail.com>
2025-06-22 16:26:04 +0300

    #SL added algorithmic diffucalt

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
@@ -1,6 +1,9 @@
 class Solution {
 public:
     int removeDuplicates(vector<int>& arr) {
+        // https://leetcode.com/problems/remove-duplicates-from-sorted-array/description/
+        // Алгортмическая сложность О(n)
+        // Сложность по памяти O(n)
         std::vector<int> result {arr[0]};
         for (std::size_t index{0}; index < std::size(arr); ++index) {
             if (result[std::size(result)-1] != arr[index]) {
