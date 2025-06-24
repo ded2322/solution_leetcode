bc39bd8add8be33ea1d5d7ab8961d3d847f92b5c
ded2322 <otheremails602@gmail.com>
2025-06-24 23:51:36 +0300

    #SL improved

--- 0189-rotate-array/0189-rotate-array.cpp
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -2,22 +2,10 @@ class Solution {
 public:
     void rotate(vector<int>& nums, int k) {
         // Алгоритмическая сложность O(n)
-        // Сложность по памяти O(n)
-
-        int size_arr = std::size(nums);
-        k %=  size_arr;
-
-        std::vector<int> void_arr;
-        std::size_t current_index = 0;
-        for (int index {0}; index < size_arr; ++index) {
-            if (k > 0 && (size_arr - k) < size_arr && k < size_arr) {
-                void_arr.push_back(nums[size_arr - k]);
-                --k;
-            } else {
-                void_arr.push_back(nums[current_index]);
-                ++current_index;
-            }
-        }
-        nums = void_arr; 
+        // Сложность по памяти O(1)
+        k %= std::size(nums);
+        std::reverse( nums.begin(), nums.end() );
+        std::reverse( nums.begin(), nums.begin() +  k  );
+        std::reverse( nums.begin() + k, nums.end() );
     }
 };
