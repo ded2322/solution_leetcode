64fe52aa3e2331612c4d665a02a49de58910f3ee
Михаил <151318767+ded2322@users.noreply.github.com>
2025-07-16 23:32:23 +0300

    Time: 23 ms (87.08%), Space: 70.9 MB (95.9%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.cpp
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -1,6 +1,8 @@
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
+        // Алгоритмическая сложность O(n log n)
+        // Сложность по памяти О(log n)
         std::sort(nums.begin(), nums.end());
 
         for(std::size_t index {0}; index < nums.size() - 1; ++index) {
