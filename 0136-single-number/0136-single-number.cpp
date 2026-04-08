f16b853ccf95204d68795a7e7dcc92436fc27f7d
Михаил <151318767+ded2322@users.noreply.github.com>
2026-04-08 23:08:59 +0300

    Time: 23 ms (5.61%), Space: 24.5 MB (7.66%) - LeetHub

---
+++ 0136-single-number/0136-single-number.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+    int output_nums{ 0 };
+    std::map<int, int> cout_input;
+    for (int num : nums) {
+        if (cout_input.find(num) == cout_input.end())
+            cout_input[num] = 1;
+        else cout_input[num] += 1;
+    }
+
+    for (auto& [value, count] : cout_input) {
+        if (count == 1) return value;
+    }
+    return 0;
+    }
+};
