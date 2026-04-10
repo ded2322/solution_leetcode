c8bd2eb9a45bd1f23d645ac596580fa15f440af7
Михаил <151318767+ded2322@users.noreply.github.com>
2026-04-11 01:39:51 +0300

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -1,24 +1,19 @@
 class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
-            std::vector<std::vector<std::string>> output_data;
+    std::map<std::array<int,26>, std::vector<std::string>> group_anagram_map{};
 
-    std::vector <std::string> strs1 = strs;
-
-    std::unordered_map<std::string, std::vector<std::string>> group_anagram_map{};
-
-    for (int i{ 0 }; i < strs1.size(); i++) {
-        std::sort(strs1[i].begin(), strs1[i].end());
-        group_anagram_map[strs1[i]] = {};
+    for (int i{ 0 }; i < strs.size(); i++) {
+        std::array<int, 26> k{};
+        for ( char& letter : strs[i]) {
+            k [letter - 'a'] += 1;
+        }
+        group_anagram_map[k].push_back(strs[i]);
     }
 
-    for (int i{ 0 }; i < strs1.size(); i++) {
-        if (group_anagram_map.find(strs1[i]) != group_anagram_map.end())
-            group_anagram_map[strs1[i]].push_back(strs[i]);
-    }
-
-    for ( auto& [key, value] : group_anagram_map) {
-        output_data.push_back(value);
+    std::vector<std::vector<std::string>> output_data{};
+    for (auto& key : group_anagram_map) {
+        output_data.push_back(key.second);
     }
     return output_data;
     }
