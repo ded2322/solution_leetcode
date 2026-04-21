919f53f90a4eac8caa424961401e1cc724992306
Михаил <151318767+ded2322@users.noreply.github.com>
2026-04-21 22:39:17 +0300

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0374-guess-number-higher-or-lower/0374-guess-number-higher-or-lower.cpp
+++ 0374-guess-number-higher-or-lower/0374-guess-number-higher-or-lower.cpp
@@ -10,19 +10,19 @@
 class Solution {
 public:
     int guessNumber(int n) {
-        int right_position{ 0 };
-        int left_position = n;
+        int right_position = n;
+        int left_position{ 1 } ;
 
-        while (right_position < left_position ) {
+        while (left_position <= right_position) {
             int middle_position = (right_position + left_position) / 2;
 
-            int currect = gues_number(middle_position);
+            int currect = guess(middle_position);
             if (currect == -1)
-                left_position = middle_position - 1;
+                right_position = middle_position - 1;
             else if (currect == 1)
-                right_position = middle_position + 1;
+                left_position = middle_position + 1;
             else return middle_position;
         }
         return 0;
-    }   
+    }
 };
